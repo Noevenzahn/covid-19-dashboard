989a0698fcfa312dc3fc813b9bbfb85f69dde4c4
Noevenzahn <info.artuofficial@gmail.com>
2021-12-11 21:36:07 +0100

    add global map

--- components/WorldMap.js
+++ components/WorldMap.js
@@ -1,26 +1,66 @@
-import ReactMapGL from 'react-map-gl';
-import { useState } from 'react';
+import ReactMapGL, { FullscreenControl, NavigationControl, AttributionControl, Source, Layer } from 'react-map-gl';
+import { useEffect, useState } from 'react';
+import { fetcher } from '../lib/Fetcher';
 
 export default function WorldMap() {
 
-  const initialState = {
-    viewport: {
-      width: '100vw',
-      height: '100vh',
-      latitude: 27,
-      longitude: 10,
-      zoom: 2.25
+  const [geojson, setGeojson] = useState();
+
+  useEffect(() => {
+    fetcher("https://disease.sh/v3/covid-19/countries")
+      .then((data) => setGeojson({
+        type: 'FeatureCollection',
+        features: data
+      }))
+  }, [])
+
+  useEffect(() => {
+    const resize = () => {
+      setViewport(viewport => { return {...viewport, width: "100vw", height: "100vh"}})
+    }
+    window.addEventListener("resize", resize);
+
+    return function cleanupListener() {
+      window.removeEventListener("resize", resize)
+    }
+  }, [])
+
+  const layerStyle = {
+    id: 'circles',
+    type: 'circle',
+    paint: {
+      'circle-opacity': 0.75,
+      'circle-stroke-width': 1,
+      'circle-radius': 4,
+      'circle-color': '#FFEB3B'
     }
   };
 
-  const [state, setState] = useState(initialState)
+  const [viewport, setViewport] = useState({
+    latitude: 27,
+    longitude: 10,
+    zoom: 2.25
+  })
 
+  if (!geojson) return <div>loading...</div>
   return (
     <ReactMapGL
+      width="100vw"
+      height="100vh"
+      onViewportChange={setViewport}
+      {...viewport}
+      attributionControl={false}
+
       mapStyle="mapbox://styles/mapbox/dark-v10"
-      mapboxApiAccessToken="pk.eyJ1IjoiajAzZXBoIiwiYSI6ImNreDB0eXVtMDFhNTUybnA4d2R0MXp0bzkifQ.fREP65CSCE02YF1QM6ayHg"
-      onViewportChange={(viewport) => setState({ viewport })}
-      {...state.viewport}
-    />
+      mapboxApiAccessToken="pk.eyJ1IjoiajAzZXBoIiwiYSI6ImNreDFyM2E4ZjFrcjIyb3A4Z2M5bGVpNGsifQ.Xs9GQEe28Uup1LYNrO09cQ"
+    >
+      <AttributionControl compact={false} style={{ bottom: 0, right: 0 }} />
+      <FullscreenControl />
+      <NavigationControl style={{ right: 0 }} />
+
+      <Source id="points" type="geojson" data={geojson}>
+        <Layer {...layerStyle} />
+      </Source>
+    </ReactMapGL>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
 import dynamic from 'next/dynamic';
-import Head from 'next/head';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const WorldMap = dynamic(() => import('../components/WorldMap'), {
   ssr: false
 });
 
 export default function Home() {
-  
+
   return (
-    <>
-      <Head>
-        <link href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css" rel="stylesheet" />
-      </Head>
-      <WorldMap />
-    </>
+    <WorldMap />
   )
 } 
