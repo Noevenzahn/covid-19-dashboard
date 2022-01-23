bbb1dfe1cd6aed7c4c21f575d04424c7daf81a7b
Noevenzahn <info.artuofficial@gmail.com>
2022-01-23 13:19:30 +0100

    remove unused code

--- components/MainChart.js
+++ components/MainChart.js
@@ -46,26 +46,8 @@ export default function MainChart({ covidData }) {
       },
     },
   };
-  const optionsStackedBar = {
-    indexAxis: "y",
-    scales: {
-      x: {
-        stacked: false,
-        ticks: {
-          beginAtZero: true,
-        },
-      },
-      y: {
-        stacked: true,
-      },
-    },
-  };
 
   const mainChartData = {
-    labels: [],
-    datasets: [],
-  };
-  const vaccinationChartData = {
     labels: [],
     datasets: [],
   };
@@ -78,7 +60,10 @@ export default function MainChart({ covidData }) {
           type: "line",
           label: "Cases",
           data: covidData.casesHistory.data.map((casesData) => {
-            return { x: casesData.date.substring(0, 10), y: casesData.cases };
+            return {
+              x: casesData.date,
+              y: casesData.cases,
+            };
           }),
           borderColor: "#fc3228",
           backgroundColor: "#fc3228",
@@ -89,7 +74,7 @@ export default function MainChart({ covidData }) {
           label: "Deaths",
           data: covidData.deathsHistory.data.map((deathsData) => {
             return {
-              x: deathsData.date.substring(0, 10),
+              x: deathsData.date,
               y: deathsData.deaths,
             };
           }),
@@ -100,95 +85,8 @@ export default function MainChart({ covidData }) {
     };
   }
 
-  if (covidData && covidData.vaccinations) {
-    vaccinationChartData = {
-      labels: [],
-      datasets: [
-        {
-          label: "Unvaccinated",
-          data: [
-            { x: 100 - 100 * covidData.vaccinations.data.quotes.total, y: 1 },
-          ],
-          borderColor: "red",
-          borderWidth: 1,
-          backgroundColor: "transparent",
-        },
-        {
-          label: "3 Vaccinations",
-          data: [
-            {
-              x:
-                100 *
-                covidData.vaccinations.data.boosterVaccination.quotes.total,
-              y: 1,
-            },
-          ],
-          borderColor: "green",
-          borderWidth: 1,
-          backgroundColor: "transparent",
-        },
-        {
-          label: "2 Vaccinations",
-          data: [
-            {
-              x:
-                100 *
-                covidData.vaccinations.data.secondVaccination.quotes.total,
-              y: 1,
-            },
-          ],
-          borderColor: "blue",
-          borderWidth: 1,
-          backgroundColor: "transparent",
-        },
-        {
-          label: "1 Vaccinations",
-          data: [{ x: 100 * covidData.vaccinations.data.quotes.total, y: 1 }],
-          borderColor: "yellow",
-          borderWidth: 1,
-          backgroundColor: "transparent",
-        },
-      ],
-    };
-  }
-
-  if (!covidData) return <div>loading...</div>;
   return (
     <>
-      {/* <div className={styles.number__group}>
-        <div className={styles.number__subGroup}>
-          <div className={styles.number__element}>
-            <p className={styles.label}>Week Incidence</p>
-            <p className={styles.value}>
-              {Math.round(covidData.overview.weekIncidence).toLocaleString()}
-            </p>
-          </div>
-          <div className={styles.number__element}>
-            <p className={styles.label}>Cases</p>
-            <p className={styles.value}>
-              {covidData.overview.cases.toLocaleString()}
-            </p>
-          </div>
-        </div>
-        <div className={styles.number__subGroup}>
-          <div className={styles.number__element}>
-            <p className={styles.label}>Deaths</p>
-            <p className={styles.value}>
-              {covidData.overview.deaths.toLocaleString()}
-            </p>
-          </div>
-          <div className={styles.number__element}>
-            <p className={styles.label}>Recovered</p>
-            <p className={styles.value}>
-              {covidData.overview.recovered.toLocaleString()}
-            </p>
-          </div>
-        </div>
-      </div> */}
-
-      {/* <div>
-        <Bar options={optionsStackedBar} data={vaccinationChartData} />
-      </div> */}
       <div id={styles.mainChart} className={styles.dashboard__element}>
         <Chart type="bar" options={options} data={mainChartData} />
       </div>
